6d343f115a648d98b4f68937348086d48fc85bf0
Rishi Selvakumaran <rishi.selvakumaran@gmail.com>
2026-06-03 08:45:14 -0500

    Revive all software projects, update skills section, add skill logos, and fix .gitignore

--- src/components/Skills/skills.component.jsx
+++ src/components/Skills/skills.component.jsx
@@ -1,5 +1,24 @@
 /* eslint-disable */
 import React from 'react'
+import AnsibleLogo from "../../assets/images/Ansible_logo.png"
+import CICDLogo from "../../assets/images/CI_CD_logo.jpeg"
+import CosmosDBLogo from "../../assets/images/CosmosDB_logo.png"
+import D3jsLogo from "../../assets/images/D3_js.jpeg"
+import DynamoDBLogo from "../../assets/images/DynamoDB_logo.jpg"
+import FlaskLogo from "../../assets/images/Flask.jpeg"
+import GrafanaLogo from "../../assets/images/Grafana.jpeg"
+import HelmLogo from "../../assets/images/HELM.jpg"
+import KustoLogo from "../../assets/images/Kusto_logo.png"
+import LLMRAGLogo from "../../assets/images/LLM_RAG_logo.jpeg"
+import Neo4jLogo from "../../assets/images/Neo4J.jpeg"
+import LangGraphLogo from "../../assets/images/langgraph-color.png"
+import MicroserviceLogo from "../../assets/images/microservice_icon.png"
+import TransformerLogo from "../../assets/images/transformer-model-icon-6855215-512.png"
+import JenkinsLogo from "../../assets/images/jenkins-logo.svg"
+
+// Uniform icon style for all skill icons
+const iconStyle = { width: '56px', height: '56px', objectFit: 'contain' };
+
 const Skills = () => {
     return(
         <section id="cta" class="main special">
@@ -9,36 +28,36 @@ const Skills = () => {
             </header>
                 <ul class="features">
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png"/>
+                    <img style={iconStyle} src="https://img.icons8.com/color/96/000000/java-coffee-cup-logo.png"/>
                     <h3>Java</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/python.png"/>
+                    <img style={iconStyle} src="https://img.icons8.com/color/96/000000/python.png"/>
                     <h3>Python</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=55251&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"/>
                     <h3>C#</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=40669&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"/>
                     <h3>C++</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/javascript.png"/>
-                    <img src="https://img.icons8.com/color/48/000000/typescript.png"/>
+                    <img style={iconStyle} src="https://img.icons8.com/color/96/000000/javascript.png"/>
+                    <img style={iconStyle} src="https://img.icons8.com/color/96/000000/typescript.png"/>
                     <h3>JS / TypeScript</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/sql.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azuresqldatabase/azuresqldatabase-original.svg"/>
                     <h3>SQL</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=NfbyHexzVEDk&format=png&color=000000"/>
+                    <img style={iconStyle} src={KustoLogo}/>
                     <h3>Kusto (KQL)</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=79750&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"/>
                     <h3>Bash</h3>
                 </li>
                 </ul>
@@ -46,27 +65,27 @@ const Skills = () => {
                 <h2>AI / ML</h2>
                 <ul class="features">
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=jH4BpkMnRrU5&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg"/>
                     <h3>PyTorch</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=n3QRpDA7KZ7P&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg"/>
                     <h3>TensorFlow</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=FBO05Dys9QCg&format=png&color=000000"/>
+                    <img style={iconStyle} src={LangGraphLogo}/>
                     <h3>LangChain / LangGraph</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=kTuxVYRKeKEY&format=png&color=000000"/>
+                    <img style={iconStyle} src={LLMRAGLogo}/>
                     <h3>LLM Agents / RAG</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=63764&format=png&color=000000"/>
+                    <img style={iconStyle} src={TransformerLogo}/>
                     <h3>Transformers</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=8rKdRqZFLurS&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/neo4j/neo4j-original.svg"/>
                     <h3>Qdrant / Neo4j</h3>
                 </li>
                 </ul>
@@ -74,29 +93,29 @@ const Skills = () => {
                 <h2>Cloud & Infrastructure</h2>
                 <ul class="features">
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/amazon-web-services.png"/>
+                    <img style={iconStyle} src="https://img.icons8.com/color/96/000000/amazon-web-services.png"/>
                     <h3>AWS</h3>
                     <p style={{fontSize: '0.8em', margin: 0}}>Lambda, EC2, S3, Kinesis, Step Functions, EMR, Glue, IAM, CloudFormation</p>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=VLKafOkk3sBX&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg"/>
                     <h3>Azure</h3>
                     <p style={{fontSize: '0.8em', margin: 0}}>AI Search, Durable Functions, Kusto</p>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/kubernetes.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-original.svg"/>
                     <h3>Kubernetes</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/docker.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"/>
                     <h3>Docker</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=cdYUlRaag9G9&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/terraform/terraform-original.svg"/>
                     <h3>Terraform</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=3iAGnbWTsFJB&format=png&color=000000"/>
+                    <img style={iconStyle} src={HelmLogo}/>
                     <h3>Helm</h3>
                 </li>
                 </ul>
@@ -104,28 +123,28 @@ const Skills = () => {
                 <h2>Systems & DevOps</h2>
                 <ul class="features">
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=21888&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg"/>
                     <h3>REST APIs / GraphQL</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=65342&format=png&color=000000"/>
+                    <img style={iconStyle} src={MicroserviceLogo}/>
                     <h3>Microservices</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=105575&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg"/>
                     <h3>CI/CD</h3>
                     <p style={{fontSize: '0.8em', margin: 0}}>Jenkins, Azure DevOps</p>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=AZOZNnY73haj&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"/>
                     <h3>Git</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=9OGIyU8hrxRs&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/grafana/grafana-original.svg"/>
                     <h3>Grafana</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=YO3YqSaTOu5K&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ansible/ansible-original.svg"/>
                     <h3>Ansible</h3>
                 </li>
                 </ul>
@@ -133,31 +152,31 @@ const Skills = () => {
                 <h2>Frameworks</h2>
                 <ul class="features">
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/spring-logo.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg"/>
                     <h3>Spring</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/nodejs.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"/>
                     <h3>Node.js</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=MHcMYcnSHJTj&format=png&color=000000"/>
+                    <img style={iconStyle} src={FlaskLogo}/>
                     <h3>Flask</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/plasticine/48/000000/react.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"/>
                     <h3>React.js</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=MWiBjkuHeMVq&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"/>
                     <h3>Next.js</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/angularjs.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg"/>
                     <h3>Angular</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=bzf0DqjXFHIW&format=png&color=000000"/>
+                    <img style={iconStyle} src={D3jsLogo}/>
                     <h3>D3.js</h3>
                 </li>
                 </ul>
@@ -165,23 +184,23 @@ const Skills = () => {
                 <h2>Databases</h2>
                 <ul class="features">
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=aBBDHRawqFKQ&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cosmosdb/cosmosdb-original.svg"/>
                     <h3>CosmosDB</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/mongodb.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"/>
                     <h3>MongoDB</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/color/48/000000/oracle-logo.png"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg"/>
                     <h3>Oracle DB</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=39913&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dynamodb/dynamodb-original.svg"/>
                     <h3>DynamoDB</h3>
                 </li>
                 <li>
-                    <img src="https://img.icons8.com/?size=48&id=62452&format=png&color=000000"/>
+                    <img style={iconStyle} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-original.svg"/>
                     <h3>Firestore</h3>
                 </li>
                 </ul>
